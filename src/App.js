e9107606e079de3ee017e215efe6c2e520d1904f
Oleg Yuzvik <yuzvik.oleg@gmail.com>
2019-04-02 23:15:10 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Contact} from './components/Contact/Contact';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="app-container">
+        <header className="app-header">
+          <h1>Contact List</h1>
+          <input type="search" className="filter-input" placeholder="search..." />
         </header>
+        <Contact 
+          name="Oleg Yuzvik" 
+          phoneNumber="+972547317884" 
+          jobTitle="Front End Web Developer" 
+          companyName="Sizmek"
+          email="yuzvik.oleg@gmail.com" />
       </div>
     );
   }
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './Contact.scss';
+
+export function Contact(props) {
+    return (
+        <div className="contact">
+            <img src={props.pictureURL} alt="Contact" />
+            <div className="contact-details">
+                <img className="contact-company-logo" src={props.companyLogoURL} alt="Contact's employer company logo" />
+                <div className="contact-name">{props.name}</div>
+                <div className="contact-work-info">{props.jobTitle} | @{props.companyName}</div>
+                <div className="contact-phone">Phone Number: {props.phoneNumber}</div>
+                <div className="contact-email">{props.email}</div>
+            </div>
+        </div>
+    )
+}
+
+Contact.displayName = "Contact";
+Contact.defaultProps = {
+    pictureURL: 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y',
+    companyLogoURL: logo
+};
