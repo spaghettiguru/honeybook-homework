d4ba7c65109d37ad011c0e6102b418002024f7d4
oleg.yuzvik <oleg.yuzvik@sizmek.com>
2019-04-03 14:12:46 +0300

    network error handling

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import {ContactList} from './components/ContactList/ContactList';
 
 import './App.scss';
 
+const CONTACTS_API_URL = 'https://candidate-test.herokuapp.com/contacts';
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       contacts: null,
-      filterText: ''
+      filterText: '',
+      networkErrorOccured: false
     };
 
     this.onFilterChanged = this.onFilterChanged.bind(this);
   }
 
   async componentDidMount() {
-    const contacts = await fetch('https://candidate-test.herokuapp.com/contacts').then(response => response.json())
-    this.setState({contacts});
+    try {
+      const contacts = await fetch(CONTACTS_API_URL).then(response => response.json())
+      this.setState({contacts});
+    } catch (error) {
+      this.setState({networkErrorOccured: true});
+    }
   }
 
   render() {
@@ -34,7 +41,8 @@ class App extends Component {
         </header>
         <div className="app-content">
           {this.state.contacts && <ContactList contacts={this.state.contacts} filterText={this.state.filterText} />}
-          {!this.state.contacts && <div className="loader">Loading contacts...</div>}
+          {!this.state.contacts && !this.state.networkErrorOccured && <div className="loader">Loading contacts...</div>}
+          {this.state.networkErrorOccured && <div className="error-message">Network error has occured. Please, refresh the page to try again.</div>}
         </div>
       </div>
     );
