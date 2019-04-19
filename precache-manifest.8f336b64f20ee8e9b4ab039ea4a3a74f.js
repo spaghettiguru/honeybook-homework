299654b93fdf4d54ae4a7556152817930fd4bd18
Oleg Yuzvik <yuzvik.oleg@gmail.com>
2019-04-19 08:29:50 +0300

    Updates

---
+++ precache-manifest.8f336b64f20ee8e9b4ab039ea4a3a74f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d2a1797a9624c05a35f077225db4d04",
+    "url": "/honeybook-homework/static/media/search-icon.9d2a1797.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/honeybook-homework/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a2058235b76069b1795",
+    "url": "/honeybook-homework/static/js/runtime~main.f77d1f06.js"
+  },
+  {
+    "revision": "ea487ee9437fd2843e58",
+    "url": "/honeybook-homework/static/js/main.76d3aa16.chunk.js"
+  },
+  {
+    "revision": "e535fe44a02a039f2fed",
+    "url": "/honeybook-homework/static/js/2.2e6ecbc1.chunk.js"
+  },
+  {
+    "revision": "ea487ee9437fd2843e58",
+    "url": "/honeybook-homework/static/css/main.1a0950eb.chunk.css"
+  },
+  {
+    "revision": "5f7b61657a80ecea28acc3c4d92c55fd",
+    "url": "/honeybook-homework/index.html"
+  }
+];
